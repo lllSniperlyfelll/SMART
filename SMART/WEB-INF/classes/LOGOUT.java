9e154ac22f49a7e07efc8d6301d18bcefa64088d
Rait Pravleen Singh <36415182+lllSniperlyfelll@users.noreply.github.com>
2019-02-28 21:21:56 +0530

    Added logout feature

---
+++ SMART/WEB-INF/classes/LOGOUT.java
@@ -0,0 +1,32 @@
+package Logoutpack;
+
+import java.util.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.*;
+
+public class LOGOUT extends HttpServlet
+{
+	public void doGet(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException
+	{
+		try
+		{
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		HttpSession session=null;
+		session=request.getSession(false);
+		session.invalidate();
+		pw.println("Logged Out");
+		}
+		catch(Exception e)
+		{
+			System.out.println("Form LOGOUT->doGet : "+e);
+		}
+
+	}
+
+	public void doPost(HttpServletRequest request,HttpServletResponse response) throws ServletException, IOException
+	{
+		doGet(request,response);
+	}
+}
