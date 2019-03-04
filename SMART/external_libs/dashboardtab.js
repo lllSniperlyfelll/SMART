a42ac282aa0639f8848cea6de56ec728f3096ead
Rait Pravleen Singh <36415182+lllSniperlyfelll@users.noreply.github.com>
2019-03-04 20:21:21 +0530

    Added libs for autocorrect

--- SMART/external_libs/dashboardtab.js
+++ SMART/external_libs/dashboardtab.js
@@ -1,28 +1,32 @@
+
+
 function hello()
 {
   var height=parseFloat(document.getElementById("hgt").value);
   var weight=parseFloat(document.getElementById("wgt").value);
   var bmi=weight/height;
+
+  //document.getElementById("button").style.display = "none";
   
   if(bmi<18.5)
   {
-    document.getElementById("demo").innerHTML="BMI is "+bmi;
-    document.getElementById("demo1").innerHTML="Underweight";
+    document.getElementById("demo").innerHTML="BMI is "+bmi+" kg/m2";
+    document.getElementById("demo1").innerHTML="Your Are Underweight";
   }
   else if(bmi>18.5 && bmi<24.9)
   {
-    document.getElementById("demo").innerHTML="BMI is "+bmi;
-    document.getElementById("demo1").innerHTML="Normal";
+    document.getElementById("demo").innerHTML="BMI is "+bmi+" kg/m2";
+    document.getElementById("demo1").innerHTML="Your are Healthy ";
   }
   else if(bmi>25.0 && bmi<29.9)
   {
-    document.getElementById("demo").innerHTML="BMI is "+bmi;
-    document.getElementById("demo1").innerHTML="Overweight";
+    document.getElementById("demo").innerHTML="BMI is "+bmi+" kg/m2";
+    document.getElementById("demo1").innerHTML="Your Are Overweight";
   }
   else if(bmi>=30)
   {
-    document.getElementById("demo").innerHTML="BMI is "+bmi;
-    document.getElementById("demo1").innerHTML="Obese";
+    document.getElementById("demo").innerHTML="BMI is "+bmi+" kg/m2";
+    document.getElementById("demo1").innerHTML="Your Are Obese";
   }
 }
 
